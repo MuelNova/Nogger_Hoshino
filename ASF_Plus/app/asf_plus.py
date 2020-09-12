85f8d6dd616f9d2b275db8b3f225b8541e2decb6
NovaNoir <68760718+the-25th-Nova@users.noreply.github.com>
2020-09-13 00:21:08 +0800

    Update asf_plus.py

--- ASF_Plus/app/asf_plus.py
+++ ASF_Plus/app/asf_plus.py
@@ -1,9 +1,12 @@
 from .ASF import ASF
 
+url = '填写ASF链接在这'
+password = '填写ASF IPCPassword在这（没有则不填）'
+
 class ASF_Plus(ASF):
 	def __init__(self,bot):
 		self.bot = bot
-		super().__init__('https://asf.novanoir.cn','dalizm8881018')
+		super().__init__(url, password)
 		
 	def startBot(self,tfa=''):
 		result = f'{self.bot}: {str(self.bot_login(self.bot,tfa))}'
