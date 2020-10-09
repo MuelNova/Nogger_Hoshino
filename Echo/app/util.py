18145760a981e50e596e179149d959472b00a7ce
the-25th-Nova <mail@novanoir.cn>
2020-10-09 13:39:27 +0800

    Modularation update

--- Echo/app/util.py
+++ Echo/app/util.py
@@ -3,3 +3,6 @@ import re
 async def isReply(msg):
     return re.match('^\[CQ:reply,id=(.+?)]\[CQ:at,qq=.+?\](.*)$',msg)
     
+async def getReply(bot,id):
+    sr = await bot.get_group_msg(message_id=int(id))
+    return sr['content']
--- Echo/echo.py
+++ Echo/echo.py
@@ -1,13 +1,12 @@
 import hoshino
 from hoshino import Service
-from .app.util import isReply, isCQ
+from .app.util import isReply, getReply
 
 sv = Service('echo')
 
 @sv.on_message()
 async def echo(bot,ctx):
     result = await isReply(ctx['raw_message'])
-    if result and result.group(2).strip() == 'echo':
-        raw = await bot.get_group_msg(message_id=int(result.group(1)))
-        raw_msg = raw['content']
+    if result and result.group(2).strip().upper() == 'ECHO':
+        raw_msg = await getReply(bot,result.group(1))
         await bot.send(ctx,raw_msg.replace('&#91;','[').replace('&#93;',']'))
