9dd8c2d6ead0cabe2784a0a561c8c8e7c90ed951
the-25th-Nova <mail@novanoir.cn>
2020-10-09 14:38:04 +0800

    Change output style to fit Yoru's characteristic.

--- Echo/echo.py
+++ Echo/echo.py
@@ -1,13 +1,18 @@
+import re
 import hoshino
 from hoshino import Service
 from .app.util import isReply, getReply
 
-sv = Service('echo',help='在聊天中对CQ码进行实时转码发送')
+sv = Service('echo',help_ = '在聊天中对CQ码进行实时转码发送')
 
 @sv.on_message()
 async def echo(bot,ctx):
     result = await isReply(ctx['raw_message'])
     if result and result.group(2).strip().upper() == 'ECHO':
-        await bot.send(ctx,'えぇ、 转码？じゃ...')
         raw_msg = await getReply(bot,result.group(1))
-        await bot.send(ctx,raw_msg.replace('&#91;','[').replace('&#93;',']'))
+        raw_msg = raw_msg.replace('&#91;','[').replace('&#93;',']')
+        if re.match('\[(.*)\]',raw_msg):
+            await bot.send(ctx,' え、转码？分かりました...')
+            await bot.send(ctx,raw_msg.replace('&#91;','[').replace('&#93;',']'))
+        else:
+            await bot.send(ctx,'うむ、 这并不是我所能理解的语言呢')
