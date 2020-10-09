db7b2b6eca3c2cf8caf4f57542a1a877f66cb0be
the-25th-Nova <mail@novanoir.cn>
2020-10-09 14:10:28 +0800

    Make the output much more nijigen(

--- Echo/echo.py
+++ Echo/echo.py
@@ -2,11 +2,12 @@ import hoshino
 from hoshino import Service
 from .app.util import isReply, getReply
 
-sv = Service('echo')
+sv = Service('echo',help='在聊天中对CQ码进行实时转码发送')
 
 @sv.on_message()
 async def echo(bot,ctx):
     result = await isReply(ctx['raw_message'])
     if result and result.group(2).strip().upper() == 'ECHO':
+        await bot.send(ctx,'えぇ、 转码？じゃ...')
         raw_msg = await getReply(bot,result.group(1))
         await bot.send(ctx,raw_msg.replace('&#91;','[').replace('&#93;',']'))
