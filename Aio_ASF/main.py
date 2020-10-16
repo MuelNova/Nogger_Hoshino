b65c6d8d602931473a84c4f4f5b7a299f6d13012
the-25th-Nova <mail@novanoir.cn>
2020-10-16 08:18:54 +0800

    added basic function

--- Aio_ASF/main.py
+++ Aio_ASF/main.py
@@ -1,4 +1,24 @@
+import re
+
 from app.aiorequests import get, post
 from app.aio_asf import AioAsf, generate_url
 from app.asfbot import asfbot
+from hoshino import Service
+from nonebot import MessageSegment
 
+
+sv = Service('aio_asf')
+
+@sv.on_message()
+async def main(bot,ctx):
+    raw = ctx['raw_message'].lower().split(' ')
+    if re.match('aasf',raw[0]):
+        if len(raw) == 2:
+            b = asfbot(raw[1])
+            stat = await b.playing_Info()
+            if stat:
+                msg = stat.get('now_playing').get('game')
+                msg += MessageSegment.image(stat.get('now_playing').get('gicon')) + '\n\nRecently:\n'
+                msg.join('{}\n 2week:{}\n'.format(i.get('name'),str(i.get('playtime_2weeks')/60)) for i in stat.get('recent'))
+                await bot.send(ctx,msg)
+    
